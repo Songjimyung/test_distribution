6e1fbc8fef47b779e2c877d30e61f575f4a238d2
Choi Junyoung <nueeeng@gmail.com>
2023-05-24 11:56:48 +0900

    ✨ Movie admin페이지 변경

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from movies.models import Movie, Genre
 
-# Register your models here.
+
+@admin.register(Movie)
+class MovieDisplay(admin.ModelAdmin):
+    # list_display는 ManyToManyField 미지원
+    list_display = [
+        'title',
+        'release_date',
+        'overview',
+        'vote_average',
+        'poster_path',
+    ]
+    readonly_fields = [
+        'title',
+        'release_date',
+        'overview',
+        'vote_average',
+        'genres',
+        'poster_path',
+    ]
+    list_filter = [
+        'title',
+        'release_date',
+        'vote_average',
+        'genres',
+    ]
+    search_fields = [
+        'title',
+        'release_date',
+        'overview',
+        'vote_average',
+        'genres',
+        'poster_path',
+    ]
