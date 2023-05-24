7968d61261a2097b1c514999770b6d10c482d258
I Joo Han <juhanlee2218@gmail.com>
2023-05-24 20:13:31 +0900

    ✨ 기능 추가: 회원가입시 이메일 인증 기능 추가

--- AIA6_BackEnd/settings.py
+++ AIA6_BackEnd/settings.py
@@ -123,7 +123,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -177,3 +176,19 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+
+
+
+'''
+email을 전송하기에 필요한 세팅들 
+email_host_user가 이메일을 보내는 사람의 주소로 표기가 되고, 
+email_host_password는 gmail에서의 앱 비밀번호이다. 
+'''
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = secrets["EMAIL_HOST_USER"]
+EMAIL_HOST_PASSWORD = secrets["EMAIL_HOST_PASSWORD"]
+SERVER_EMAIL = 'djangopractest@gmail.com'
+DEFAULT_FROM_MAIL = 'id'
--- users/serializers.py
+++ users/serializers.py
@@ -6,6 +6,13 @@ from django.contrib.auth.hashers import check_password
 from reviews.models import Review
 from reviews.serializers import ReviewSerializer
 
+from django.utils.http import urlsafe_base64_encode
+from django.core.mail import EmailMessage
+from django.utils.encoding import force_bytes
+from django.urls import reverse
+from rest_framework_jwt.settings import api_settings
+
+
 # 회원가입에 필요한 serializer
 class SignUpSerializer(serializers.ModelSerializer):
     re_password = serializers.CharField(
@@ -80,8 +87,23 @@ class SignUpSerializer(serializers.ModelSerializer):
         user = User.objects.create(user_name=user_name, email=email,)
         
         user.set_password(password)
+
+        # is_active = False로 지정해주어 email 인증을 하기 전에 접속 불가능하게 설정
+        user.is_active = False
         user.save()
-        
+
+        # jwt 토큰 생성
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+        payload = jwt_payload_handler(user)
+        token = jwt_encode_handler(payload)
+
+        # email을 생성하여 보내는 부분
+        message = f"{user.email}님 링크를 클릭해 계정을 활성화 해주세요\n"
+        message += f"http://127.0.0.1:8000{reverse('user:activate', kwargs={'uidb64': urlsafe_base64_encode(force_bytes(user.pk)), 'token': token})}"
+        email = EmailMessage('test', message, to=[user.email])
+        email.send()
+
         return validated_data
 
 
