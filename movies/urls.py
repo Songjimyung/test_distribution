798d16606c2ecec6522ff8492285018b8031de75
Songjimyung <jimyung0401@gmail.com>
2023-05-26 01:42:25 +0900

    ⚡️ similarmoviesview 추가 및 파이썬 파일 추가

--- movies/urls.py
+++ movies/urls.py
@@ -4,4 +4,6 @@ from movies import views
 urlpatterns = [
     path('main/', views.MovieListView.as_view(), name='main'),
     path('recommendation/<int:movie_id>/', views.MovieDetailView.as_view(), name='recommendation'),
+    path('save/', views.SaveMoviesView.as_view(), name='csv_save'),
+    path('recommendation/', views.SimilarMoviesView.as_view(), name='movie_recommendation'),
 ]
