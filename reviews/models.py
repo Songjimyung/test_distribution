e0aa473b33c91abf72d8127c93388c3f9d6487f8
Choi Junyoung <nueeeng@gmail.com>
2023-05-26 09:56:11 +0900

    🐛 FloatField RATING_CHOICES 버그 수정
    RATING_CHOICES에서 기존 정수 부분만 있는 부분들은 정수만 남겨
    프론트 연결해서 글 작성시 평점 관련 오류 버그 픽스합니다.

--- reviews/models.py
+++ reviews/models.py
@@ -6,17 +6,17 @@ from django.urls import reverse
 
 class Review(models.Model):
     RATING_CHOICES = (
-        (0.0, 0.0), 
+        (0, 0), 
         (0.5, 0.5), 
-        (1.0, 1.0), 
+        (1, 1), 
         (1.5, 1.5),
-        (2.0, 2.0),
+        (2, 2),
         (2.5, 2.5),
-        (3.0, 3.0),
+        (3, 3),
         (3.5, 3.5),
-        (4.0, 4.0),
+        (4, 4),
         (4.5, 4.5),
-        (5.0, 5.0),
+        (5, 5),
     )
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="movies")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="users")
