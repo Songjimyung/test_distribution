a567c423f7e1c54921f7253b806e357c373bd724
Choi Junyoung <nueeeng@gmail.com>
2023-05-28 14:52:54 +0900

    🎨 Pagination 기능 추가

--- movies/urls.py
+++ movies/urls.py
@@ -3,5 +3,6 @@ from movies import views
 
 urlpatterns = [
     path('main/', views.MovieListView.as_view(), name='main'),
+    path('movie/', views.MovieListPaginatedView.as_view(), name='movie_paginated'),
     path('recommendation/<int:movie_id>/', views.MovieDetailView.as_view(), name='recommendation'),
 ]
