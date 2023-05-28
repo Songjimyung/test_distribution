f6c712289a7dbbe6ed4ed0112ec3c8a7ed6bb86d
Choi Junyoung <nueeeng@gmail.com>
2023-05-28 18:06:13 +0900

    🚑 Review rating필드 Integer로 수정
    별점 표기를 쉽게하기 위해 rating field를 IntegerField로 수정합니다

--- reviews/models.py
+++ reviews/models.py
@@ -7,21 +7,16 @@ from django.urls import reverse
 class Review(models.Model):
     RATING_CHOICES = (
         (0, 0), 
-        (0.5, 0.5), 
         (1, 1), 
-        (1.5, 1.5),
         (2, 2),
-        (2.5, 2.5),
         (3, 3),
-        (3.5, 3.5),
         (4, 4),
-        (4.5, 4.5),
         (5, 5),
     )
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="movies")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="users")
     content = models.TextField("내용", null=True)
-    rating = models.FloatField("평점", choices=RATING_CHOICES)
+    rating = models.IntegerField("평점", choices=RATING_CHOICES)
     created_at = models.DateTimeField("리뷰생성일", auto_now_add=True)
     updated_at = models.DateTimeField("리뷰변경일", auto_now=True)
     like = models.ManyToManyField(User, related_name="likes", blank=True)
